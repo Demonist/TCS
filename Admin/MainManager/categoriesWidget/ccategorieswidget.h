08e292bce10163752aecf6a24eb70d2b2374aac7
paladin8818 <nekromant8818@mail.ru>
2012-12-29 22:19:03 +0400

    Widget

---
+++ Admin/MainManager/categoriesWidget/ccategorieswidget.h
@@ -0,0 +1,31 @@
+#ifndef CCATEGORIESWIDGET_H
+#define CCATEGORIESWIDGET_H
+
+#include <QtGui>
+#include "cabstractcommonwidget.h"
+#include "ccategorydialog.h"
+
+namespace Ui {
+class CCategoriesWidget;
+}
+
+class CCategoriesWidget : public CAbstractCommonWidget
+{
+    Q_OBJECT
+private:
+    Ui::CCategoriesWidget *ui;
+
+public:
+    explicit CCategoriesWidget(QWidget *parent = 0);
+    ~CCategoriesWidget();
+    
+public slots:
+    void updateData();
+
+private slots:
+    void on_pbCategoriesAdd_clicked();
+    void on_pbCategoriesEdit_clicked();
+    void on_pbCategoriesDelete_clicked();
+};
+
+#endif // CCATEGORIESWIDGET_H
